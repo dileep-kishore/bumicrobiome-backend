1a155f13bc9d6bbb1d1bc2c9a61f1ad4d3e4cc72
Dileep Kishore <k.dileep1994@gmail.com>
2018-02-08 22:48:36 -0500

    Minor style changes

--- microbiome_api/models/user.py
+++ microbiome_api/models/user.py
@@ -2,7 +2,8 @@ from microbiome_api.extensions import db, pwd_context
 
 
 class User(db.Model):
-    """Basic user model
+    """
+        Basic user model
     """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -11,8 +12,8 @@ class User(db.Model):
     active = db.Column(db.Boolean, default=True)
 
     def __init__(self, **kwargs):
-        super(User, self).__init__(**kwargs)
+        super().__init__(**kwargs)
         self.password = pwd_context.hash(self.password)
 
     def __repr__(self):
-        return "<User %s>" % self.username
+        return f"<User name={self.username}>"
