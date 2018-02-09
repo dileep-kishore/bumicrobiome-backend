18dc66f72e984bd7e0439691db91f4e58bf42025
Dileep Kishore <k.dileep1994@gmail.com>
2018-02-08 22:48:01 -0500

    Added docker and celery extensions

--- microbiome_api/extensions.py
+++ microbiome_api/extensions.py
@@ -7,9 +7,13 @@ from flask_sqlalchemy import SQLAlchemy
 from passlib.context import CryptContext
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
+from celery import Celery
+import docker
 
 
 db = SQLAlchemy()
 jwt = JWTManager()
 ma = Marshmallow()
+cl = Celery()
+dkr = docker.from_env()  # NOTE: The docker-machine needs to be pre-configured
 pwd_context = CryptContext(schemes=['pbkdf2_sha256'], deprecated='auto')
